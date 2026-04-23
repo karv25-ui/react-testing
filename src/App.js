99335cb576eb9b6d311eff8781fec4567522aad2
karv25-ui <karvenda@gmail.com>
2026-04-23 06:53:37 +0000

    react testing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./styles.css";
 
 function App() {
   const [text, setText] = useState("Hello");
 
   function updateText() {
-    setText("Welcome"); 
+    setText("Welcome");
   }
 
   function toggleText() {
-    setText(prev => (prev === "Hello" ? "Welcome" : "Hello"));
+    setText((prev) => (prev === "Hello" ? "Welcome" : "Hello"));
   }
 
   function upperCase() {
-    setText(prev => 
-      prev === prev.toUpperCase()
-      ? prev.toLowerCase() 
-      : prev.toUpperCase()
+    setText((prev) =>
+      prev === prev.toUpperCase() ? prev.toLowerCase() : prev.toUpperCase()
     );
   }
 
   function Reset() {
     setText("Hello");
   }
+
+  function nameInput() {
+    const [name, setName] = useState("");
+  }
   return (
     <>
-    <h1>{text}</h1>
-    <button onClick={toggleText}>Toggle</button>
-    <button onClick={upperCase}>UpperCase Text</button>
-    <button onClick={Reset}>Reset Text</button>
+      <h1>{text}</h1>
+      <input placeholder="Enter Name:" />
+      <h2>Booking for: </h2>
+      <button onClick={toggleText}>Toggle</button>
+      <button onClick={upperCase}>UpperCase Text</button>
+      <button onClick={Reset}>Reset Text</button>
     </>
   );
 }
